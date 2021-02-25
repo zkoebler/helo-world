3245546d28de1e5b341276cc9ed89634a6a0d270
zkoebler <55300165+zkoebler@users.noreply.github.com>
2021-02-25 14:05:05 -0700

    Update GitDemo.java
    Added functionality to the addNumber method and set the new reverseNum variable equal to the newNumber variable.

--- GitDemo.java
+++ GitDemo.java
@@ -1,9 +1,10 @@
 /*****
- * Edit this line to have your name.
+ * Zephrom Koebler
  ******/
 public class GitDemo {
     
     private double previousNumber;
+    private double reverseNum;
     
     public GitDemo() {
         previousNumber = 1;
@@ -14,7 +15,7 @@ public class GitDemo {
     *****/
     public double addNumber(double num) {
         System.out.print("Add num to previousNumber");
-        return 0;
+        return num + previousNumber;
     }
     
     /*****
@@ -45,7 +46,7 @@ public class GitDemo {
         for(int i = 1; i <= num; ++i){
             newNumber = newNumber * i;
         }
-        
+        reverseNum = newNumber;
         return newNumber;
     }
     
